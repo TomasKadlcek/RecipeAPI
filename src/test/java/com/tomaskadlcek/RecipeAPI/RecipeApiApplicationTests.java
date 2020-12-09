7844b2b168cc5ce8def1a8014493bc3f5c97cb72
Tomas Kadlcek <kadlcek.tomas@gmail.com>
2020-12-09 12:44:38 +0100

    Initial commit

---
+++ src/test/java/com/tomaskadlcek/RecipeAPI/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomaskadlcek.RecipeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
