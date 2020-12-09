1f086a2979aab1ceda284d1b0063c3dfdff8f06f
Tomas Kadlcek <kadlcek.tomas@gmail.com>
2020-12-09 12:46:27 +0100

    adding basic spring structure

--- src/main/java/com/tomaskadlcek/RecipeAPI/RecipeApiApplication.java
+++ src/main/java/com/tomaskadlcek/RecipeAPI/RecipeApiApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecipeApiApplication {
 
+	// if on server uncomment
+
+	// extends SpringBootServletInitializer
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+//		return application.sources(MessengerApplication.class);
+//	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RecipeApiApplication.class, args);
 	}
